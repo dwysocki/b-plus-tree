49c7ada98e4944668ba004ff9293f5b890b59317
Rosnec <rosnec.dev@gmail.com>
2014-04-18 22:51:51 -0400

    Got reading/writing nodes to a RAF working, though it's only been tested for root-leaf so far.

--- src/b_plus_tree/io.clj
+++ src/b_plus_tree/io.clj
@@ -3,8 +3,37 @@
   (:require [gloss core io]
             [b-plus-tree.nodes :as nodes]))
 
-(defn seek
-  ([^java.io.RandomAccessFile raf
-    ^java.lang.long           offset]
-     (.seek raf offset)))
+(defn read-page
+  "Reads a page's worth of bytes from the RandomAccessFile at a given offset."
+  ([raf offset pagesize]
+     (let [b (byte-array pagesize)]
+       (seek raf offset)
+       (.readFully raf b)
+       b)))
 
+(defn read-node
+  "Reads the node stored in the RandomAccessFile at the given offset."
+  ([raf offset]
+     (.seek raf offset)
+     (let [type (.readByte raf)
+           [type encoding] (nodes/byte->encoding type)
+           size (.readShort raf)
+           node-bytes (byte-array size)]
+       (.readFully raf node-bytes)
+       (-> node-bytes count println)
+       (assoc (gloss.io/decode encoding (gloss.io/to-byte-buffer node-bytes))
+         :type type))))
+
+(defn write-node
+  "Writes the node to the RandomAccessFile at the given offset. Returns the
+  offset of the file after writing."
+  ([node raf offset]
+     (.seek raf offset)
+     (let [[type encoding] (nodes/type->encoding (:type node))
+           encoded-node (gloss.io/encode encoding node)
+           size (gloss.core/byte-count encoded-node)]
+       (doto raf
+         (.writeByte type)
+         (.writeShort size)
+         (.write (.array (gloss.io/contiguous encoded-node))))
+       (.getFilePointer raf))))
--- src/b_plus_tree/nodes.clj
+++ src/b_plus_tree/nodes.clj
@@ -2,62 +2,39 @@
   "Defines encodings for the different types of nodes used in the B+ Tree."
   (:require [gloss core]))
 
-(def node-types
-  [:root-leaf :root-nonleaf :internal :leaf :record])
-
-(def node-type->id
-  (into {} (map (fn [keyword id] [keyword (byte id)])
-                node-types (range))))
-
-(def id->node-type
-  (into {} (map (fn [keyword id] [id keyword])
-                node-type->id)))
+(gloss.core/defcodec- C-string
+  (gloss.core/string :ascii :delimiters ["\0"]))
 
 (gloss.core/defcodec- key-list
-  (repeated (string :ascii :length key-length)
-            :prefix (prefix :ubyte
-                            #(/ % key-length)
-                            #(* % key-length))))
+  (gloss.core/repeated C-string))
 
 (gloss.core/defcodec- child-list
-  (repeated :int64
-            :prefix (prefix :ubyte
-                            #(/ % 8)
-                            #(* % 8))))
+  (gloss.core/repeated :int64))
 
-(gloss.core/defcodec- record-list
-  (gloss.core/repeated (gloss.core/string :ascii :length val-length)
-                       :prefix (gloss.core/prefix :ubyte
-                                                  #(/ % val-length)
-                                                  #(* % key-length))))
-
-(gloss.core/defcodec root-leaf-node
+(gloss.core/defcodec root-node
   (gloss.core/ordered-map
-   :keys key-list
-   :vals val-list))
-
-(gloss.core/defcodec root-non-leaf-node
-  (gloss.core/ordered-map
-   :keys key-list
+   :nextfree :int64
+   :keys     key-list
    :children child-list))
 
-(gloss.core/defcodec internal-node
+(gloss.core/defcodec child-node
   (gloss.core/ordered-map
-   :keys key-list
+   :keys     key-list
    :children child-list))
-
-(gloss.core/defcodec leaf-node
-  (gloss.core/ordered-map
-   :keys key-list
-   :vals val-list))
 
 (gloss.core/defcodec record-node
-  (gloss.core/ordered-map
-   :vals val-list))
+  C-string)
 
-(gloss.core/defcodec btree-node
-  (gloss.core/header
-   node-type
-   {:root-leaf root-leaf-node, :root-non-leaf root-non-leaf-node,
-    :internal  internal-node,  :leaf          leaf-node}
-   :type))
+(def type->encoding
+  {:root-leaf    [0 root-node  ]
+   :root-nonleaf [1 root-node  ]
+   :internal     [2 child-node ]
+   :leaf         [3 child-node ]
+   :record       [4 record-node]})
+
+(def byte->encoding
+  {0 [:root-leaf    root-node  ]
+   1 [:root-nonleaf root-node  ]
+   2 [:internal     child-node ]
+   3 [:leaf         child-node ]
+   4 [:record       record-node]})
