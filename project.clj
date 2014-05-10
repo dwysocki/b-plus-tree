d2b5ef7c6aba5fb4b832f48161e475c662bc694c
Rosnec <rosnec.dev@gmail.com>
2014-05-09 20:35:51 -0400

    fixing version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b-plus-tree "0.1.7"
+(defproject b-plus-tree "0.1.6"
   :description "A B+ Tree implemented in Clojure."
   :url "https://github.com/Rosnec/b-plus-tree"
   :license {:name "MIT License"
